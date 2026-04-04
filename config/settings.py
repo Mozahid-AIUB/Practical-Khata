3a7f338dd9690639cb717d082aa967a86469115a
mozahid <mozahidislam343@gmail.com>
2026-04-04 22:53:05 +0600

    Fix deployment

--- config/settings.py
+++ config/settings.py
@@ -68,8 +68,8 @@ DATABASES = {
 # ── Static & Media ────────────────────
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
